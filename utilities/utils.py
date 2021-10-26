012d07d8711db64669207c147c9f44d1f9938124
Aji <ajikarunia@gmail.com>
2021-10-25 19:05:19 -0700

    cleaner code

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,10 +1,18 @@
 def modparse(local_dict):
     modname = local_dict['__name__']
-    modname = modname.split(".")[0]
+    if "." in modname:
+        modname = modname.split(".")[1]
     packname = local_dict['__package__']
     return modname, packname
-def moduleinfo(modname, packname):
+def moduleinfo(local_dict):
+    modname, packname = modparse(local_dict)
     print(f'[Package, Module]: {packname} {modname}')
+
+def doc(x):
+    """
+    Print the docstring
+    """
+    print(x.__doc__)
 
 import config
 if config.PRINT_MODULE_INFO:
