311bf988ddf17f371b01dec5d73e1ebeeb645ed9
Aji <ajikarunia@gmail.com>
2021-10-27 16:07:59 -0700

    global path variables

--- myports.py
+++ myports.py
@@ -6,4 +6,10 @@ import pandas as pd
 
 
 #User
+import tools.excelio as eio
 from tools.utils import doc, ls
+
+src = Path('~/Projects/pynet')
+data = src / 'data'
+lip = data / 'sars-cov-2-LiP'
+multi = data / 'multi-proteomics'
