b7e35bf016ab22ea5e83ec27b90ed8e358ef485d
Aji <ajikarunia@gmail.com>
2021-10-28 11:42:47 -0700

    more devtools

--- dev.py
+++ dev.py
@@ -8,6 +8,7 @@ import pandas as pd
 #User
 import tools.excelio as eio
 from tools.utils import doc, ls
+from tools.myitertools import exhaust, forp
 
 home = Path.home()
 src = home / 'Projects/pynet'
