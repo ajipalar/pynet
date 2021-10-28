d83399ed6c0b670d414fc94d573610aadd92515f
Aji <ajikarunia@gmail.com>
2021-10-27 20:14:28 -0700

    Generators for reading excel sheets

--- tools/excelio.py
+++ tools/excelio.py
@@ -5,6 +5,7 @@ import sys
 
 """
 Reading and parsing excel files
+pf pure function. Function has no side effects
 """
 
 parser = argparse.ArgumentParser()
@@ -17,19 +18,58 @@ args = parser.parse_args()
 def colnames(p):
     return pd.read_excel(p).columns
 
-def get_xlsx_file_paths(dirpath:Path):
+def gen_xlsx_fpaths(p):
+    """
+    :param p pathlib path
+    returns a generator
+    """
     for f in p.iterdir():
         if f.suffix == '.xlsx':
             yield f
-           
+
+def gen_sheets(fpath):
+    """
+    :param fpath file path
+    returns a generator of sheetnames
+    """
+    ef = pd.ExcelFile(fpath)
+    for sheet in ef.sheet_names:
+        yield sheet
+
+def tree_excel(p):
+    for xlsx in get_xlsx_file_paths(p):
+        print(f'{xlsx}')
+        for sheet in get_sheets(xlsx):
+            print(f'\t{sheet}')
+
+def condf(f, condition, *args):
+    """
+    :f funciton
+    :condition 
+    :*args args for f
+    Executes f if condition
+    """
+    return f(*args) if condition else None
+ 
+
+def treef(p, f=lambda pathy : None, *args): 
+    """
+    Recursivley applies a function to files in a directory like linux tree
+    """
+    for pathy in p.iterdir():
+        if pathy.is_file():
+            f(*args)
+        if pathy.is_dir():
+            treef(pathy, f, *args)
+
+
+
+
 def print_all_colnames(path):
    p = Path(args.d)
    for f in p.iterdir():
       if f.suffix == '.xlsx':
           print(colnames(f)) 
-
-
-def 
 
 if args.d:
    print_all_colnames(args.d)
