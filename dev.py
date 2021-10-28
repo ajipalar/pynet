377c75565fa08946f3c5e109f95b01da7ffe631f
Aji <ajikarunia@gmail.com>
2021-10-27 20:57:39 -0700

    myports => dev

---
+++ dev.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+import pandas as pd
+
+
+
+#User
+import tools.excelio as eio
+from tools.utils import doc, ls
+
+home = Path.home()
+src = home / 'Projects/pynet'
+data = src / 'data'
+lip = data / 'sars-cov-2-LiP'
+multi = data / 'multi-proteomics'
