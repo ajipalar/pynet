5654463d7b9ce8076656c3094ef8fc4476060877
Aji <ajikarunia@gmail.com>
2021-10-29 15:51:10 -0700

    functions to parse lip data

--- tools/io.py
+++ tools/io.py
@@ -157,6 +157,89 @@ def print_all_colnames(path):
       if f.suffix == '.xlsx':
           print(colnames(f)) 
 
+### Read .xls files using iterators instead of pandas ###
+def gen_xls(xlsfile: FilePath) -> Iterator[str]:
+    with open(xlsfile, 'r') as f:
+        for line in f:
+            yield f.readline()
+
+def parse_lip_line(line: str) List[str]:
+    return list(i.strip() for i in line.split('\t'))
+
+def gen_parsed_lip_xls(xlsfile: FilePath) -> Iterator[List[str]]:
+    lip_gen = gen_xls(xlsfile)
+    return (parse_lip_line(i) for i in lip_gen)
+
+def filter_lines(line_list: List[str], column_positions: List[int]) -> List[str]:
+    new_list = []
+    for pos in column_positions:
+        new_list.append(line_list[pos])
+    return new_list
+    
+def gen_filter_xls_columns(it: Iterator[List[str]], column_positions: List[int]) -> Iterator[List[str]]:
+    for stripped_str_list in it:
+        yield filter_lines(stripped_str_list, column_positions)
+
+def desired_columns() -> List[str]:
+    columns = ['R.Condition', 'R.FileName', 'R.Label', 'R.Replicate', 
+               'PG.ProteinAccessions', 'PG.ProteinGroups',  'PG.Qvalue', 'PG.Quantity', 
+               'PEP.IsProteinGroupSpecific', 'PEP.IsProteotypic', 'PEP.NrOfMissedCleavages', 'PEP.PeptidePosition', 'PEP.StrippedSequence', 'PEP.DigestType - [Trypsin/P]',   
+               'EG.iRTPredicted', 'EG.ModifiedPeptide', 'EG.ModifiedSequence', 'EG.Qvalue', 
+               'FG.Charge', 'FG.LabeledSequence', 'FG.PrecMz', 'FG.MS2RawQuantity'] 
+    return columns
+
+def build_position_dict(l: List[str]) -> Dict:
+    d = {}
+    for i, k in enumerate(l):
+        d[k] = i
+    return d
+
+def check_desired_columns_in_header(header_cols: List[str], desired_cols: List[str]):
+    for col in desired_cols:
+        assert col in header_cols 
+
+def desired_positions(input_header_list: List[str], columns: List[str]) -> List[int]:
+    
+
+
+
+def get_header(xlsfile: FilePath) -> List[str]:
+    with open(xlsfile, 'r') as f:
+        header = f.readline()
+        header = header.split('\t')
+        header = list(i.strip() for i in header)
+        return header
+
+
+def remove_irrelevant_columns(it: Iterator[str]) -> Iterator[str]:
+    headers = ['R.Condition', 'R.FileName', 'R.Fraction', 'R.Label', 'R.Replicate', 
+               'PG.ProteinAccessions', 'PG.ProteinGroups', 'PG.Cscore', 'PG.Pvalue', 'PG.Qvalue', 'PG.RunEvidenceCount', 'PG.Quantity', 
+               'PEP.AllOccurringProteinAccessions', 'PEP.GroupingKey', 'PEP.GroupingKeyType', 'PEP.IsProteinGroupSpecific', 
+               'PEP.IsProteotypic', 'PEP.NrOfMissedCleavages', 'PEP.PeptidePosition', 'PEP.StrippedSequence', 
+               'PEP.DigestType - [Trypsin/P]', 'PEP.Rank', 'PEP.RunEvidenceCount', 'PEP.UsedForProteinGroupQuantity', 
+               'EG.IntPIMID', 'EG.iRTPredicted', 'EG.IsDecoy', 'EG.ModifiedPeptide', 'EG.ModifiedSequence', 'EG.UserGroup', 'EG.Workflow', 'EG.IsUserPeak', 'EG.IsVerified', 'EG.Qvalue', 'EG.ApexRT', 'EG.iRTEmpirical', 'EG.RTPredicted', 
+               'EG.AvgProfileQvalue', 'EG.MaxProfileQvalue', 'EG.MinProfileQvalue', 'EG.PercentileQvalue', 'EG.ReferenceQuantity (Settings)', 
+               'EG.TargetQuantity (Settings)', 'EG.TotalQuantity (Settings)', 'EG.UsedForPeptideQuantity', 'EG.UsedForProteinGroupQuantity', 'EG.Cscore', 
+               'FG.Charge', 'FG.IntMID', 'FG.LabeledSequence', 'FG.PrecMz', 'FG.PrecMzCalibrated', 'FG.MS2Quantity', 'FG.MS2RawQuantity', 'FG.Quantity']
+    positions = build_position_dict(headers)
+    header = list(next(it)).split('\t')
+    
+    assert len(header) == 55
+
+
+def xls_shape(xlsfile: FilePath) -> Tuple[int, int]:
+    r = 0
+    c = 0
+    with open(xlsfile, 'r') as f:
+        c = len(f.readline().split('\t'))
+        for line in f:
+            r+=1
+    return (r, c)
+        
+    
+
+
+
 if args.d:
    print_all_colnames(args.d)
    
