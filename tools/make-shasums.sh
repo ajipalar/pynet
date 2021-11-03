57c39ee9dc754f5ae57b9c0f9617e148b1886fca
ajipalar <aji.palar@ucsf.edu>
2021-11-03 11:12:56 -0700

    deleted tools

--- tools/make-shasums.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for file in "$1"/*
-do
-shasum -a 256 $file
-done
--- tools/myitertools.py
+++
@@ -1,6 +0,0 @@
-def exhaust(it):
-    for i in it: pass
-
-def forp(it):
-    """Prints the contents of the iterator, consuming it"""
-    exhaust(map(print, it))
