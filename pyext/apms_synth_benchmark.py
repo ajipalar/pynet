2063362a12b29c661043fb85351b85479192dd47
ajipalar <aji.palar@ucsf.edu>
2021-11-10 09:26:14 -0800

    synthetic benchmark for apms

---
+++ pyext/apms_synth_benchmark.py
@@ -0,0 +1,40 @@
+"""
+
+
+"""
+import graph_tool as gt
+from dev import *
+
+apms = pd.read_excel(apms_gordon)
+
+
+#Represent the model
+
+
+#Define the prior
+
+
+#Define the likelihood
+#L(G, 
+def apms_like
+
+#Define the scoring function
+
+
+#Define a graph enumerator
+
+
+#Create synthetic data from the inverse of the likelihood function
+
+
+#Prior predictive check on synthetic data
+
+
+
+
+#Enumerate all graphs and see how they score
+
+
+
+
+
