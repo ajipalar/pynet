7bb307603765cdb8d348a5d76001b398ff2aecea
ajipalar <aji.palar@ucsf.edu>
2022-01-05 15:46:06 -0800

    Make a well formatted checksum file

--- utility/make-shasums.sh
+++ utility/make-shasums.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 function mysha256 {
+  # dir must be a directory
   local dir=$1
-  if [[ -d $dir ]]; then
-    for file in "$dir"/*
-    do
-      if [[ -f $file ]]; then
-      shasum -a 256 $file
-      fi
-    done
-  fi
+  [[ -d $dir ]] || exit 1
+
+  local files=$(ls $dir)
+  for file in $files; do
+    if [[ -f $dir/$file ]]; then
+      shasum -a 256 $dir/$file
+    fi
+  done
   return 0
 }
 
-if [[ -d $1 ]]; then
-  mysha256 $1
-elif [[ -f $1 ]]; then
-  shasum -a 256 $1
+input=$(echo $1 | sed 's/\/$//')
+
+if [[ -d $input ]]; then
+  mysha256 $input > $input/checksumfile
+  sed '/checksumfile/d' <$input/checksumfile > $input/checksumfile.tmp
+  cat $input/checksumfile.tmp > $input/checksumfile
+  rm $input/checksumfile.tmp
+  exit 0
+elif [[ -f $input ]]; then
+  shasum -a 256 $input
+  exit 0
 else
   echo "Error"
-  echo "The first positional argument '$1'"
+  echo "The first positional argument '$input'"
   echo "is neither a file nor a director"
   exit 1
 fi
 
-exit 0
+exit 1
