1ba1c336e5e7a49322d3cccfb0df35e5e6236551
ajipalar <aji.palar@ucsf.edu>
2022-01-06 00:21:14 -0800

    where the program is run

---
+++ main.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from utility import data_setup as ds
+from utility.data_setup import ls
+
+home = Path(".")
+pyext = home / "pyext"
+data = pyext / "data"
+corum = data / "corum"
+ecoli = data / "ecoli"
+genetic_screen = data / "genetic-screen"
+sars_cov_2_lip = data / "sars-cov-2-LiP"
+sars_cov_2_ppi = data / "sars-cov-2-ppi"
+multi_proteomics = data / "multi-proteomics"
+
+utility = home / "utility"
+
