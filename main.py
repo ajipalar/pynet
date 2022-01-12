3d4c9f1d58585262556c701b7232e1fdcc48ecab
ajipalar <aji.palar@ucsf.edu>
2022-01-11 23:52:40 -0800

    command line in python

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ assert pyversion[0:5] == '3.9.7'
 from pathlib import Path
 from utility import data_setup as ds
 from utility.data_setup import ls
+from pyext.repl import cl, pwd
+import test.test_graph
 
 home = Path(".")
 pyext = home / "pyext"
@@ -19,4 +21,6 @@ sars_cov_2_ppi = data / "sars-cov-2-ppi"
 multi_proteomics = data / "multi-proteomics"
 
 utility = home / "utility"
+
+
 
