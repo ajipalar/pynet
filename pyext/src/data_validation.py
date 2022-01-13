3f4718041b8367c98261e8a67d1692ecf86e05ac
ajipalar <aji.palar@ucsf.edu>
2022-01-13 15:19:36 -0800

    module for validating data input

---
+++ pyext/src/data_validation.py
@@ -0,0 +1,17 @@
+"""
+validate checksums
+
+Data                 Model.obj
+ |                       |
+ v                       v
+Data stream          model.data stream
+ |           sort        |
+ v                       v
+Sorted                  Sorted
+Data                    model 
+stream                  data 
+ |          compare      |
+ v                       v
+    validated data input
+
+"""
