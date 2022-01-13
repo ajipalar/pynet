323ea492590143c9f2d534f94c33594f21df8529
ajipalar <aji.palar@ucsf.edu>
2022-01-12 19:51:09 -0800

    defines function compositions using type checking

---
+++ pyext/src/compose.py
@@ -0,0 +1,21 @@
+from inspect import signature
+from typing import Any, Callable 
+from typedefs import Function, Parameters
+
+import typing
+
+def parameters(f: Function):
+    return signature(f).parameters
+
+def Domain(f: Function) -> Parameters:
+    return signature(f).parameters
+
+
+def Codomain(f: Function):
+    return reveal_type(signature(f).return_annotation)
+
+def composable(f: Function) -> set[Function]:
+    pass
+
+y = Domain(Codomain)
+reveal_type(y)
