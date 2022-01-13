5e4f526f5084a5ae446a9232003c766dd9f6015f
ajipalar <aji.palar@ucsf.edu>
2022-01-12 19:49:40 -0800

    python alias for command line

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ assert pyversion[0:5] == '3.9.7'
 
 
 from utility import data_setup as ds
-from utility.data_setup import ls
-from pyext.repl import cl, pwd
+from pyext.repl import cl, ls, pwd
 import test.test_graph
 
 home = Path(".")
@@ -27,6 +26,15 @@ sars_cov_2_ppi = data / "sars-cov-2-ppi"
 multi_proteomics = data / "multi-proteomics"
 
 utility = home / "utility"
+
+
+
+
+
+
+
+
+
 
 
 
