b6c0d5db3a12e796c5b6662ba9a0a31120388754
ajipalar <aji.palar@ucsf.edu>
2022-01-12 19:49:52 -0800

    ls working

--- pyext/src/repl.py
+++ pyext/src/repl.py
@@ -1,4 +1,7 @@
+"""Unix command line alias to use in python"""
 import os
+import pathlib
+from pathlib import Path, PosixPath
 
 class CL(object):
     """An alias for clear in python"""
@@ -11,5 +14,15 @@ class PWD(object):
     def __repr__(self):
         return os.getcwd()
 
+class LS(object):
+    """An alias for ls in python."""
+    def __repr__(self):
+        cwd: PosixPath = Path.cwd() 
+        for path in cwd.iterdir(): print(path)
+        return ""
+
+
+
 cl = CL()
 pwd = PWD()
+ls = LS()
