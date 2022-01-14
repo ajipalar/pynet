577a232f8da59307b2deddfccd77895b096ef681
ajipalar <aji.palar@ucsf.edu>
2022-01-14 15:46:47 -0800

    syntax

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ assert version.parse(pyversion) >= version.parse('3.9.7')
 
 
 import pyext.src.graph as graph
-import pyext.src.get_network_overlap as get_network_overlap 
+#import pyext.src.get_network_overlap as get_network_overlap 
 import pyext.src.ii as ii
 import pyext.src.sampling as sampling
 import pyext.src.score as score
-import pyext.src.typdefs as typdefs
+import pyext.src.typedefs as typedefs
 import pyext.src.utils as utils
 import pyext.src.vis as vis
 
@@ -39,6 +39,8 @@ sars_cov_2_ppi = data / "sars-cov-2-ppi"
 multi_proteomics = data / "multi-proteomics"
 
 utility = home / "utility"
+
+
 
 
 
