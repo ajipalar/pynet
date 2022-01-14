0cbd7e2c1e615512bad52d7880b9f8701ad31982
ajipalar <aji.palar@ucsf.edu>
2022-01-13 17:17:24 -0800

    refactor

--- main.py
+++ main.py
@@ -3,17 +3,30 @@ import jax
 import numpy
 import matplotlib
 import matplotlib.pyplot as plt
+from packaging import version
 from pathlib import Path
 import scipy
 import sys
 
-pyversion = sys.version
-assert pyversion[0:5] == '3.9.7'
+pyversion = sys.version.split('|')[0]
+assert version.parse(pyversion) >= version.parse('3.9.7')
 
+
+import pyext.src.graph as graph
+import pyext.src.get_network_overlap as get_network_overlap 
+import pyext.src.ii as ii
+import pyext.src.sampling as sampling
+import pyext.src.score as score
+import pyext.src.typdefs as typdefs
+import pyext.src.utils as utils
+import pyext.src.vis as vis
+
+import test.test_graph
 
 from utility import data_setup as ds
-from pyext.repl import cl, ls, pwd
-import test.test_graph
+from utility.meta import pipe
+
+from pyext.src.repl import cl, ls, pwd
 
 home = Path(".")
 pyext = home / "pyext"
