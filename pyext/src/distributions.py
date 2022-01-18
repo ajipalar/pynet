2f9da01a8e24b240e45e37f24452d96a00cbb235
ajipalar <aji.palar@ucsf.edu>
2022-01-18 14:46:09 -0800

    A distributions module

---
+++ pyext/src/distributions.py
@@ -0,0 +1,42 @@
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+class Distribution():
+    def __init__():
+        pass
+
+    def logpdf():
+        pass
+
+    def pdf():
+        pass
+
+    def logcdf():
+        pass
+
+    def cdf():
+        pass
+
+    def logpmf():
+        pass
+
+    def pmf():
+        pass
+
+    def ppf():
+        pass
+
+    def rv():
+        pass
+
+    def sf():
+        pass
+
+    def isf():
+        pass
+
+    
+
+
+
