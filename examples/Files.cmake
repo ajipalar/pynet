a7b7720970edfcad1d00f845a22dc5e484f1ca4a
ajipalar <aji.palar@ucsf.edu>
2022-01-22 04:53:24 -0800

    should be ignored

--- examples/Files.cmake
+++ examples/Files.cmake
@@ -1,3 +1,3 @@
-set(pyfiles "")
+set(pyfiles "LiP_EDA.py;eight_schools_numpyro.py;example.py;functional.py;halves-sum.py;igm.py;jax-demo.py;jaxtest.py;pyro1.py;sample_normal.py;sampling.py;stan_example.py")
 set(cppfiles "")
 set(cudafiles "")
