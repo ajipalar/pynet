3b95f6721782d300e48ae6f6e6b71b4e5b07164b
ajipalar <aji.palar@ucsf.edu>
2022-01-26 18:53:34 -0800

    module level naming check

---
+++ test/test_pynet.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import IMP.test
+import IMP.algebra
+try:
+    import IMP.pynet
+except ModuleNotFoundError:
+    import pyext.src
+import io
+import os
+import math
+
+class Tests(IMP.test.TestCase):
+    module = IMP.pynet
+
+    def test_magnitude(self):
+        """Write the test cast, print statemetns ok"""
+        pass
+
+    def test_function_names(self):
+        exceptions = []
+        words = []
+        self.assertFunctionNames(self.module, exceptions, words)
+
+    def test_class_names(self):
+        exceptions = []
+        words = []
+        self.assertClassNames(self.module, exceptions, words)
+
+    
+
+
+if __name__ == '__main__':
+    IMP.test.main()
