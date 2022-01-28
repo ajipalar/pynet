11ec72c627063e76fe54d974081316fae3b022a9
ajipalar <aji.palar@ucsf.edu>
2022-01-27 19:09:03 -0800

    locally passing

--- pyext/src/benchmark.py
+++ pyext/src/benchmark.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import pyext.src.data_in
-from project_paths import benchmarks
+import numpy as np
+try:
+    from IMP.pynet.typedefs import Array
+except ModuleNotFoundError:
+    from pyext.src.typedefs import Array
 
 def write_example_benchmark():
     """Write an an example benchmark file
@@ -12,3 +16,27 @@ def write_example_benchmark():
 
     with open(fpath, 'w') as f:
         line = "interaction-id\t"
+
+
+# per sample test statistics
+# TP known
+# FN known
+# TN unkown
+# FP unkown
+
+def accuracy(y_ref: Array, y_pred: Array) -> float:
+    """
+    @param y_ref 1d array like
+    @param y_pred 1d array like
+    @return accuracy_score 
+    """
+    assert y_ref.shape == y_pred.shape
+    assert y_ref.ndim == 1
+    assert y_pred.ndim == 1
+    assert y_ref.dtype == y_pred.dtype
+
+    score: Array  = y_ref ==  y_pred  # element wise comparison 
+    accuracy_score: float  = np.sum(score)
+    accuracy_score /= len(score)
+    return accuracy_score
+    
