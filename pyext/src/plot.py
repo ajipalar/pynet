2412f32a818e82af7660fc35b4e6e104c90d0f6a
ajipalar <aji.palar@ucsf.edu>
2022-02-04 11:36:34 -0800

    plotting helper functions

---
+++ pyext/src/plot.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from typing import Any
+try:
+    from IMP.pynet.typedefs import Array1d
+except ModuleNotFoundError:
+    from pyext.src.typedefs import Array1d
+
+
+def marginal(x: Array1d, 
+             xlabel: str = 'x',
+             ylabel: str = 'freq',
+             bins=50, 
+             **kwargs):
+    x = np.array(x)
+    plt.hist(x, bins=bins, **kwargs)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+def scatter(x: Array1d, 
+            y: Array1d,
+            xlabel: str = 'x',
+            ylabel: str = 'y',
+            **kwargs):
+    plt.scatter(x, y)
+    plt.show()
