748d9732b5820531794089d59bdd806f43010fdf
ajipalar <aji.palar@ucsf.edu>
2022-03-07 16:45:44 -0800

    prune dead code

--- pyext/src/ais.py
+++ pyext/src/ais.py
@@ -248,19 +248,6 @@ def generic_ais(key : PRNGKey = None,
     samples, log_weights = jax.lax.fori_loop(0, n_samples, n_samples_loop_body, (samples, log_weights))
     return samples, log_weights
 
-def jit_generic_ais_as_ais_example(mu=10, sig=2, n_samples=600, n_inter=60, n_giibs_steps=5):
-    p_n = jax.random.normal
-    
-    f0_pdf = partial(f0_pdf, mu=mu, sig=sig) 
-    fj_pdf = partial(fj_pdf, f0_pdf=f0_pdf, fn_pdf=fn_pdf)
-
-    f0_logpdf = partial(f0_logpdf, mu=mu, sig=sig)
-    fj_logpdf = partial(fj_logpdf, f0_logpdf=f0_logpdf, fn_logpdf=fn_logpdf)
-
-
-
-
-
 # AIS in the context of sqr models
 def get_phi_tilde(phi : Matrix, gamma : float) -> Matrix:
     phi_diag = phi.diagonal()
@@ -268,13 +255,6 @@ def get_phi_tilde(phi : Matrix, gamma : float) -> Matrix:
     phi_tilde = nblib.set_diag(phi_tilde, phi_diag)
     return phi_tilde
 
-
-def jit_compile_sqr_ais():
-    """jit compiles the annealed importance sampling into a single kernal"""
-    fj_pdf = fg.f0  # (xsi, eta1, eta2) -> float 
-    pass
-
-    
 
 def get_sqr_ais_weights(key : PRNGKey, 
                n_samples : Dimension, 
@@ -369,162 +349,7 @@ def get_sqr_ais_weights(key : PRNGKey,
         samples.at[j].set(x)
         weights = weights.at[j].set(jnp.exp(w))
 
-
-
-
-
-def sampler(key, n_samples : int, 
-            n_inter, 
-            betas, 
-            x,
-            f0_pdf : Callable,
-            fj_pdf : Callable,
-            fn_pdf : Callable,
-            T,  # the transition rule
-            ) -> tuple[Array, Array]:
-
-    """Annealed Importance Sampling by Neal.
-       Obtain expectations or an estimate of the partition function
-       of the distribution of interest p0 = 1/z0 * f0_pdf begining
-       with the distribution p_n = 1/z_n * fn_pdf using intermediate
-       distributions p_j = 1/z_j * fj_pdf
-    """
-
-
-    return samples, weights
-
-#samples, weights = do_ais(key, n_samples, betas, n_inter, x)
-#a = 1/np.sum(weights) * np.sum(weights * samples)
-
-def ais_poisson_sqr(n_inter : int,  # the number of intermediate distributions
-                    f0_pdf,
-                    fn_pdf,
-                    fj_pdf,
-                    phi,
-                    phi_diag,
-                    theta):
-
-    gamma_arr = jnp.arange(n_inter)  # from 0 to n_inter - 1
-    
-    def T(x, f, n_steps=10):
-        for t in range(n_steps):
-
-            x_prime = x + np.random.randn()
-
-            a = f(x_prime) / f(x)
-
-            if np.random.rand() < a:
-                x = x_prime
-
-    for zi in range(n_inter):
-        gamma = gamma_arr[zi]
-        theta_tilde = theta * gamma
-        phi_tilde = phi_off * gamma + phi_diag
-
-        
-
-
-
-    return weights, samples
-
-def ais_example(mu, sig, n_samples=600, n_inter=60, n_gibbs_steps=5):
-    """The example from Augstinus Kristiadi's blog"""
-
-    def T(x, f, n_steps=10):
-        for t in range(n_steps):
-
-            x_prime = x + np.random.randn()
-
-            a = f(x_prime) / f(x)
-
-            if np.random.rand() < a:
-                x = x_prime
-        return x
-
-    p_n = st.norm(0, 1)
-    
-    f0_pdf = partial(f0_pdf, mu=mu, sig=sig) 
-    fj_pdf = partial(fj_pdf, f0_pdf=f0_pdf, fn_pdf=fn_pdf)
-
-    f0_logpdf = partial(f0_logpdf, mu=mu, sig=sig)
-    fj_logpdf = partial(fj_logpdf, f0_logpdf=f0_logpdf, fn_logpdf=fn_logpdf)
-
-    #x = np.arange(-10, 5, 0.1)
-    betas = np.linspace(0, 1, n_inter)
-
-    samples = np.zeros(n_samples)
-    weights = np.zeros(n_samples)
-
-    for t in range(n_samples):
-        x = p_n.rvs()
-        w = 1
-
-        for n in range(1, len(betas)):
-            x = T(x, lambda x: fj_pdf(x, betas[n]), n_steps=n_gibbs_steps)
-
-            fn = fj_pdf(x, betas[n])
-            fnm1 = fj_pdf(x, betas[n-1])
-            if fn == 0 or fnm1 == 0:
-                print(f"t {t}, n {n}\nx {x}, w {w}\nfn {fn}, fnm1 {fnm1}\nbetas[n] {betas[n]},betas[n-1] {betas[n-1]}")
-                return None
-
-            w += np.log(fj_pdf(x, betas[n])) - np.log(fj_pdf(x, betas[n - 1]))
-
-        samples[t] = x
-        weights[t] = np.exp(w)
-
-    return samples, weights
-
-def gibbs_ais_sqr(key):
-    key, k1 = jax.random.split(key)
-
-    samples = jnp.zeros(n_samples)
-    weights = jnp.zeros(n_samples)
-
-
-    # 
-
-    x = jax
-
-    return samples, weights
-
-def gibbs_generic_ais(key,
-              f_m,
-              fj_pdf,
-              f0_pdf,
-              T,
-              m_inter : Dimension,
-              nsamples : Dimension,
-              f0_pdfargs=[],
-              f0_pdfkwargs={},
-              fj_pdfargs=[],
-              fj_pdfkwargs={},
-              f_margs=[],
-              f_mkwargs={}) -> tuple[DeviceArray]:
-    """params:
-         f_m : the starting distribution
-         fj_pdf : the intermediate distributions
-         f0_pdf : the distribution of interest
-         n_inter : the number of intermediate distributions from [1 to n_inter]
-         n_samples : number of samples and weights to return
-    """
-
-    # initiate the samples and weights
-    samples = jnp.zeros(n_samples)
-    weights = jnp.zeros(n_samples)
-
-
-    # sample the 
-
-    key, k1 = jax.random.split(key, 2)
-    x = jax.random.exponential(k1, shape=(nsamples, ))
-
-
-
-
-    return samples, weights
-
-def get_mean(samples : Array = None, weights : Array = None)-> float:
+def get_mean__j(samples : Array = None, weights : Array = None)-> float:
     """params:
         samples: 1d array
         weights: 1d array 
