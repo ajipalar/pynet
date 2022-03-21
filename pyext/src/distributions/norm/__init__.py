e87b9350739224300f81e463facd74c682f47260
ajipalar <aji.palar@ucsf.edu>
2022-03-21 11:00:19 -0700

    type annotations in src end up in mypy stubs

--- pyext/src/distributions/norm/__init__.py
+++ pyext/src/distributions/norm/__init__.py
@@ -3,7 +3,10 @@ import jax.numpy as jnp
 import jax.scipy as jsp
 import numpy as np
 
+from typing import Any, Callable
 
+
+pdf: Callable[[Any], float]
 pdf = jax.scipy.stats.norm.pdf
 lpdf = jax.scipy.stats.norm.logpdf
 cdf = jsp.stats.norm.cdf
