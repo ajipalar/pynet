d51e6499abe5a36e4e0a7729e03d935eb58c88fa
ajipalar <aji.palar@ucsf.edu>
2022-03-28 16:47:35 -0700

    minus vector operator locally passed

--- pyext/src/poissonsqr.py
+++ pyext/src/poissonsqr.py
@@ -5,7 +5,7 @@ import numpy as np
 from functools import partial
 from typing import Callable as f
 from typing import Protocol
-from .typedefs import Dimension
+from .typedefs import Dimension, JitFunc
 import collections
                     # variable value -> Array index
 n_samples: int      # [1, n_samples] -> [0, n_samples)
@@ -13,6 +13,7 @@ n_prey: Dimension
 n_replicates: int
 n_interpolating: int
 
+pynet_jax_fdtype = jnp.float32 
 
 # samples, weights = model.sample.ais(n_samples, n_interpolating)
 
@@ -39,39 +40,92 @@ get = Get(
     lambda theta, phi, x, i: theta[i] + 2)
 
 
+def remove_ith_entry__s(arr_l: Dimension) -> JitFunc:
+    i_eq_0__j = i_eq_0__s(arr_l)
+    i_eq_arr_l__j = i_eq_arr_l__s(arr_l)
+    zero_lt_i_lt_arr_l__j = zero_lt_i_lt_arr_l__s(arr_l)
+    branch2__j = branch2__s(arr_l)
+    
+    def remove_ith_entry__j(arr, i):
+        out_arr = jax.lax.cond(
+            i==0,
+            lambda a, b: i_eq_0__j(arr=a),
+            lambda a, b: branch2__j(arr=a, i=b),
+            *(arr, i))
+        return out_arr
+
+    return remove_ith_entry__j
+
+
 
 def remove_ith_entry(arr, arr_l: Dimension, i: int):
     """Array has at least two elements"""
-
-    
-    def i_eq_0(arr, arr_l: Dimension, i: int):
-        """ i=0 -> [1, arr_l)"""
-        out_arr = jnp.zeros(arr_l -1)
-        out_arr = arr[1:arr_l]
-        return out_arr
-
-    def i_eq_arr_l(arr, arr_l: Dimension, i: int):
-        """ i=arr_l-1 -> [0, arr_l - 1)"""
-        out_arr = jnp.zeros(arr_l -1)
-        out_arr = arr[0:arr_l - 1]
-        return out_arr
-
-    def zero_lt_i_lt_arr_l(arr, arr_l: Dimension, i: int):
-        """ 0 < i < arr_l -> [0, i) c [i+1, arr_l) """
-        out_arr = jnp.zeros(arr_l -1)
-        out_arr = out_arr.at[0:i].set(arr[0:i])
-        out_arr = out_arr.at[i:arr_l-1].set(arr[i+1:arr_l])
-        return out_arr
-
-    p1 = i == 0
-    
-    def branch2(arr, arr_l, i):
-        p2 = i == arr_l
-        out_arr = jax.lax.cond(p2, i_eq_arr_l, zero_lt_i_lt_arr_l, (arr, arr_l, i))
-        return out_arr
-
-    out_arr = jax.lax.cond(p1, i_eq_0, branch2, (arr, arr_l, i))
+    out_arr = jax.lax.cond(i==0, lambda a, b, c: i_eq_0(a, b), branch2, *(arr, arr_l, i))
     return out_arr
+    
+#def i_eq_0(arr, arr_l: Dimension, i: int):
+def i_eq_0(arr, arr_l): 
+    """ i=0 -> [1, arr_l)"""
+    out_arr = jnp.zeros(arr_l -1, dtype=pynet_jax_fdtype)
+    out_arr = arr[1:arr_l]
+    return out_arr
+
+def i_eq_0__s(arr_l: Dimension):
+    i_eq_0__j = partial(i_eq_0, arr_l=arr_l)
+    return i_eq_0__j
+
+#def i_eq_arr_l(arr, arr_l: Dimension, i: int):
+def i_eq_arr_l(arr, arr_l):
+    """ i=arr_l-1 -> [0, arr_l - 1)"""
+    out_arr = jnp.zeros(arr_l -1, dtype=pynet_jax_fdtype)
+    out_arr = arr[0:arr_l - 1]
+    return out_arr
+
+def i_eq_arr_l__s(arr_l: Dimension):
+    i_eq_arr_l__j = partial(i_eq_arr_l, arr_l=arr_l)
+    return i_eq_arr_l__j
+
+
+
+#def zero_lt_i_lt_arr_l(arr, arr_l: Dimension, i: int):
+def zero_lt_i_lt_arr_l(arr, arr_l, i):
+    """ 0 < i < arr_l -> [0, i) c [i+1, arr_l) """
+    out_arr = jnp.zeros(arr_l-1, dtype=pynet_jax_fdtype)
+    # out_arr = out_arr.at[0:i].set(arr[0:i])
+
+    # Not differentiable, bounds unknown at trace time
+    out_arr, tmp  = jax.lax.fori_loop(
+        0, i,
+        lambda j, t: (t[0].at[j].set(t[1][j]), t[1]),  
+        (out_arr, arr)) 
+
+    out_arr, tmp  = jax.lax.fori_loop(
+        i+1, arr_l,
+        lambda j, t: (t[0].at[j-1].set(t[1][j]), t[1]),  (out_arr, arr)) 
+        
+    return out_arr
+
+def zero_lt_i_lt_arr_l__s(arr_l):
+    return partial(zero_lt_i_lt_arr_l, arr_l=arr_l)
+
+
+#def branch2(arr, arr_l, i):
+def branch2(arr, arr_l, i):
+    out_arr = jax.lax.cond(i==arr_l-1, lambda a, b, c: i_eq_arr_l(a, b), zero_lt_i_lt_arr_l, *(arr, arr_l, i))
+    return out_arr
+
+def branch2__s(arr_l: Dimension):
+    i_eq_arr_l__j = i_eq_arr_l__s(arr_l)
+    zero_lt_i_lt_arr_l__j = zero_lt_i_lt_arr_l__s(arr_l)
+    def branch2__j(arr, i):
+        return jax.lax.cond(
+            i==arr_l-1,
+            lambda a, b : i_eq_arr_l__j(arr=a),
+            lambda a, b : zero_lt_i_lt_arr_l__j(arr=a, i=b),
+            *(arr, i))
+
+    return branch2__j
+    
 
 
 
---
+++ test/local/test_poissonsqr.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+import IMP.test
+import IMP.algebra
+import os
+import math
+import collections
+
+from ..src import poissonsqr as td
+import pyext.src.poissonsqr as src_module
+
+class Tests(td.PoissUnitTests):
+    """Derived class for Poisson SQR Model unit tests"""
+    src = src_module
+    rtol=1e-5
+    atol=1e-5
+    decimal = 7
+
+    kwds = collections.namedtuple(
+            'KWDS', ['rtol', 'atol', 'decimal'])(rtol, atol, decimal)
+
+
+
+if __name__ == '__main__':
+    IMP.test.main()
