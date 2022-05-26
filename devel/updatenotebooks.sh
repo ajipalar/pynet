08b7cadf3d75830609f0e7acad89a7d7ed675403
aji <ajikarunia@gmail.com>
2022-05-26 14:41:54 -0700

    command line tool to update paired notebooks

---
+++ devel/updatenotebooks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+output_prefix="[updatenotebooks]"
+
+echo "$output_prefix updating paired notebooks"
+for notebook in $( ls *.ipynb);
+do
+        strlen=${#notebook}
+        suffend=$strlen-1
+        suffbegin=$strlen-5
+        source_file="${notebook:0:$suffbegin-1}.py"
+        jupytext --update --to notebook $source_file
+
+
+done
+
+echo "$output_prefix done"
+
+
