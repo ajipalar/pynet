79fc9149a0b06268964a27ceaecfe3cc757d3dc6
ajipalar <aji.palar@ucsf.edu>
2022-09-19 18:58:59 -0700

    Wishart random variates

--- pyext/src/pynet_rng.py
+++ pyext/src/pynet_rng.py
@@ -7,7 +7,7 @@ import jax.scipy as jsp
 import jax.scipy.linalg as lin
 
 
-def positive_definite(key, n):
+def is_positive_definite(key, n):
     """
     Generate a real n-by-n positive definite matrix
 
@@ -40,28 +40,79 @@ def chi2(key, k, shape=None, dtype=None):
 
     return 2 * jax.random.gamma(key, k / 2, shape=shape, dtype=dtype)
 
-
-
-
-def wishart(key, V, p, n):
+def standard_wishart(key, p, n):
     """
-    Draw a p x p scatter matrix from the Wishart Distribution. O(n^3)
-    If V is invertible then S is invertable
+    
+    Compute A drawn from a standard wishart. Analagous to standard normal.
+    n > p-1
 
-    No runtime checks are performed
+    A is a matrix whose diagonal are sqrt(chi2)
+    Top diagonal are zeros
+    Off lower diagonal are normal
+
+    Args:
+      key : PRNGKey
+          Number of variates to generate
+      p : The dimension of the scatter matrix
+      n : The degrees of freedom
+
+    Retruns:
+
+    Source:
+    """
+    # Normal Elements for off diagonal
+
+    keys = jax.random.split(key, num = 2)
+    n_tril = p * (p-1) // 2
+    covariances = jax.random.normal(keys[0], shape=(n_tril,)) 
+
+    # sqrt(chi-square) of n-i + 1 for diagonal elements
+
+    ks = n - jnp.arange(1, p+1) + 1
+    variances = jnp.sqrt(chi2(keys[1], ks))
+
+    # Create an A matrix
+    A = jnp.zeros((p, p))
+
+    # Input the covariances
+    
+    tril_idx = jnp.tril_indices(p, k=-1) # diagonal offset k
+    A = A.at[tril_idx].set(covariances)
+
+    # Input the variances
+
+    diag_idx = jnp.diag_indices(p)
+    A = A.at[diag_idx].set(variances)
+
+    return A
+
+
+def _wishart(key, C, n, p):
+    """
+    Draw random samples from a Wishart distribution
 
     Args:
       V : the p x p scale matrix
       p : the rank of the matrix
-      n : degrees of freedom with n <= p 
+      n  : The degrees of freedom of the matrix
     Returns:
       S : the p x p scatter matrix
     """
+
+    A = standard_wishart(key, p, n)  
+    CA = jnp.dot(C, A)
+    A = jnp.dot(CA, CA.T)
+    return A
+
+def wishart(key, V, n, p):
+    """
+    Draw random samples from Wishart Distribution
+    """
+    C = jsp.linalg.cholesky(V, lower=True)
+    S = _wishart(key, C, n, p)
+    return S
+
+
+
     
-    keys = jax.random.split(key, )
-
-    c_squared = jax.random
-
-
-
 
