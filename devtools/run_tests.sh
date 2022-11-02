0d88f13c0a88e3625603d519e257df357ff410a5
ajipalar <aji.palar@ucsf.edu>
2022-11-02 13:40:30 -0700

    run local tests

---
+++ devtools/run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+module load Sali
+conda activate pynetp3.10.0
+
+pwd
+prefix='test/local/'
+for pytest in $(ls $prefix'test_'*.py); do
+	string=${pytest%.py}
+	module=${string#"$prefix"}
+	echo "Running $module"
+	python3 -m 'test.local.'$module
+done
